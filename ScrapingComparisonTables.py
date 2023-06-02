0a5ebfec54a7e034a36d6bf43a2edbe85e3c2c91
22amj19 <22amj19@ms.dendai.ac.jp>
2023-06-02 16:13:29 +0900

    不要なインポートの削除

--- ScrapingComparisonTables.py
+++ ScrapingComparisonTables.py
@@ -2,7 +2,6 @@
 import urllib.request
 from urllib.parse import quote
 from bs4 import BeautifulSoup
-import time
 
 USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36" #おまじない
 
